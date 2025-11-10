b7fd2a6caea740630472ff31487d86660b4a9dcd
Saito Tsutomu <tsutomu.saito@moai-lab.jp>
2025-11-10 14:43:31 +0900

    0.2.0

--- src/simple_tsp/tsp.py
+++ src/simple_tsp/tsp.py
@@ -17,7 +17,7 @@ def distance(lat1: float, lon1: float, lat2: float, lon2: float) -> float:
     """Great circle distance (km) between two points"""
     r = 6371
     ry1, rx1, ry2, rx2 = radians(lat1), radians(lon1), radians(lat2), radians(lon2)
-    return r * acos(sin(ry1) * sin(ry2) + cos(ry1) * cos(ry2) * cos(rx1 - rx2))
+    return r * acos(min(1.0, sin(ry1) * sin(ry2) + cos(ry1) * cos(ry2) * cos(rx1 - rx2)))
 
 
 def _get_location_or_create(m: Model, locations: dict[Key, Location], key: Key) -> Location:
@@ -44,7 +44,11 @@ def tsp(
 def tsp(distances: list[list[int]], depot: int = 0, max_iterations: int = DEFAULT_MAX_ITERATIONS) -> list[str]: ...
 
 
-def tsp(distances: DistanceDict, depot: Key = 0, max_iterations: int = DEFAULT_MAX_ITERATIONS) -> list[Key]:  # type: ignore[misc]
+def tsp(
+    distances: DistanceDict,
+    depot: Key = 0,
+    max_iterations: int = DEFAULT_MAX_ITERATIONS,
+) -> list[int] | list[str]:
     """Solve a traveling salesman problem and return a list of cities
 
     :param distances: Dictionary or list of distances
@@ -54,13 +58,14 @@ def tsp(distances: DistanceDict, depot: Key = 0, max_iterations: int = DEFAULT_M
     """
     m = Model()
     m.add_vehicle_type(1)
-    locations = {depot: m.add_depot(x=0, y=0, name=str(depot))}
+    locations: dict[Key, Location] = {depot: m.add_depot(x=0, y=0, name=str(depot))}
     if isinstance(distances, dict):
         for (i, j), distance in distances.items():
             location_i = _get_location_or_create(m, locations, i)
             location_j = _get_location_or_create(m, locations, j)
             m.add_edge(location_i, location_j, distance=distance)
     else:
+        assert isinstance(distances, list)  # noqa: S101
         n = len(distances)
         for i in range(1, n):
             m.add_client(x=0, y=0, name=str(i))
@@ -68,9 +73,11 @@ def tsp(distances: DistanceDict, depot: Key = 0, max_iterations: int = DEFAULT_M
             for j in range(n):
                 if i != j:
                     m.add_edge(m.locations[i], m.locations[j], distance=distances[i][j])
+
     result = m.solve(MaxIterations(max_iterations), display=False)
     if result.best.distance() >= 2**52:
         return []
     route = result.best.routes()[0]
     key_type = type(depot)
-    return [depot, *(key_type(m.locations[i].name) for i in route.visits())]
+    cities_ = (key_type(m.locations[i].name) for i in route.visits())
+    return [depot, *(cities_)]  # type: ignore[bad-assignment]
--- src/tests/test_tsp.py
+++ src/tests/test_tsp.py
@@ -1,9 +1,30 @@
 # flake8: noqa: S101
 """テスト"""
 
+from math import isclose
+
 import pytest
 
-from simple_tsp import tsp
+from simple_tsp import distance, tsp
+
+
+def test_distance_near() -> None:
+    """distanceのテスト"""
+    d = distance(36, 135, 36.1, 134.9)
+    assert isclose(d, 14.300, abs_tol=0.001)
+
+
+def test_distance_far() -> None:
+    """distanceのテスト"""
+    d = distance(36, 135, 35, 136)
+    assert isclose(d, 143.38, abs_tol=0.01)
+
+
+def test_distance_same_location() -> None:
+    """distanceの同一地点のテスト"""
+    lat, lon = 36.172522266885586, 135
+    d = distance(lat, lon, lat, lon)
+    assert isclose(d, 0, abs_tol=0.00000001)
 
 
 @pytest.mark.parametrize(
@@ -14,6 +35,6 @@ from simple_tsp import tsp
         ([[0, 9, 1, 9], [1, 0, 9, 9], [9, 9, 0, 1], [9, 1, 9, 0]], 0, [0, 2, 3, 1]),
     ],
 )
-def test_tsp(distances, depot, expected) -> None:  # noqa: ANN001
+def test_tsp(distances: dict, depot: int, expected: list[int]) -> None:
     """tspのテスト"""
     assert tsp(distances, depot) == expected
